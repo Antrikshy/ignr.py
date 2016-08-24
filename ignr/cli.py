f3453351d6411a3c72e44b0a8665a5a2b40c762f
Antriksh Yadav <antrikshy@gmail.com>
2016-08-24 14:20:36 +0800

    Added search option

--- ignr/cli.py
+++ ignr/cli.py
@@ -6,15 +6,27 @@ parser = argparse.ArgumentParser()
 # Accept list instruction, search instruction, OR create instruction
 task = parser.add_mutually_exclusive_group(required=True)
 task.add_argument('--list', '-l', action='store_true', help='list all available .gitignore templates')
+task.add_argument('--search', '-s', dest='s_term', metavar='TECH', help='search for term in list of available templates')
 task.add_argument('--new', '-n', dest='n_stack', metavar='TECH', nargs='+', help='space-separated technologies - "macOS Node Sass"')
 task.add_argument('--preview', '-p', dest='p_stack', metavar='TECH', nargs='+', help='preview for space-separated technologies')
 
 args = parser.parse_args()
 
-if args.list:
+# --list or --search
+if args.list or args.s_term:
     template_list = api.get_template_list()
-    print '\n'.join(template_list)
 
+    if args.list:
+        print '\n'.join(template_list)
+    
+    if args.s_term:
+        for template in template_list:
+            if args.s_term.lower() in template.lower():
+                print template
+
+    sys.exit(0)
+
+# --new or --preview
 else:
     preview_needed = args.p_stack is not None
     gi_stack = args.p_stack or args.n_stack
