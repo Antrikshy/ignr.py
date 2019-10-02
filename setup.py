56922144bd149e060950d864e46d1a3033bffbef
Alex Ward <alxwrd@googlemail.com>
2019-10-02 13:54:19 +0100

    switch from `scripts` to `console_scripts` entry
    `console_scripts` allows for python functions from a package to be used
    as an entry point for an application. the `scripts` entry allows for
    anything to be used, such as a shell script.
    
    as the script is python anyway, using `console_scripts` is ok. it also
    has the added benefit of making it windows compatible.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='https://github.com/Antrikshy/ignr',
     author='Antriksh Yadav',
     author_email='antrikshy@gmail.com',
-    scripts=['bin/ignr'],
+    entry_points={'console_scripts': ['ignr=ignr:cli']},
     packages=['ignr'],
     license='MIT',
     keywords='git gitignore gitignore.io developer'
