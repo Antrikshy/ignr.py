74f542aa32a1d09fca55dfad4a88bb4605c9e41e
Antriksh Yadav <antrikshy@gmail.com>
2022-06-11 13:54:33 -0700

    Fixed console_scripts to make ignr runnable without python3 -m on Unix

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ setup(
     name='ignr',
     description='Python command line client to download' \
                 '.gitignore templates from gitignore.io',
-    version='2.0',
+    version='2.2',
     url='https://github.com/Antrikshy/ignr',
     author='Antriksh Yadav',
     author_email='antrikshy@gmail.com',
-    entry_points={'console_scripts': ['ignr=ignr.cli']},
+    entry_points={'console_scripts': ['ignr=ignr.cli:__main__']},
     packages=['ignr'],
     license='MIT',
-    keywords='git gitignore gitignore.io developer'
+    keywords='git gitignore gitignore.io developer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Version Control :: Git'
+    ]
 )
